0072023ebb6adc18ef27517f473b9948e674f5b0
Ambulong <zeng.ambulong@gmail.com>
2018-08-15 10:54:10 +0800

    u

--- vsplatego.sh
+++ vsplatego.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-service mysqld start || service mysqld start && mysql -u root -ptoor -e 'create database typecho;';
+service mysqld start || service mysql start && mysql -u root -ptoor -e 'create database typecho;';
 chmod -R 777 /var/www/html;
